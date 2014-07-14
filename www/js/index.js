e3b3405badce6572ca6bc28f5353472679311af4
agarciaGIT <garcia.alberto.crespo@gmail.com>
2014-07-14 14:30:28 -0400

    try

--- www/js/index.js
+++ www/js/index.js
@@ -55,7 +55,7 @@ var app = {
     }, // result contains any message sent from the plugin call
     successHandler: function(result) {
         alert('Push Callback Success! Result = '+result)
-        document.getElementById("p1").innerHTML = "New text:" + result;
+        document.getElementById("msg").innerHTML = "New text:" + result;
 
     },
     errorHandler:function(error) {
